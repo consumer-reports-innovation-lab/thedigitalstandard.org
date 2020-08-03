fbd86c9d7f8820cb3faabf474e331601f4b330cd
Nathaniel Fostan <nathanielfostan@gmail.com>
2020-08-03 00:25:33 -0500

    animations

--- src/js/scrollMagicScenes.js
+++ src/js/scrollMagicScenes.js
@@ -15,6 +15,12 @@ $(document).ready(function () {
     triggerElement: '#shield-sreen-blue',
     reverse: false
   })
+    .setClassToggle('.intro .animate-text-fade-up-delay', 'text-fade-up-delay')
+    .addTo(controller);
+  var homeScene = new ScrollMagic.Scene({
+    triggerElement: '#shield-sreen-blue',
+    reverse: false
+  })
     .setClassToggle('.intro .animate-blue-bg', 'animate-blue-bg-fill')
     .addTo(controller);
   var homeScene = new ScrollMagic.Scene({
@@ -56,7 +62,94 @@ $(document).ready(function () {
 
 
 
+  // Begin Overview Animations
+  // Section One
+  var overviewScene = new ScrollMagic.Scene({
+    triggerElement: '#learn-how',
+    reverse: false
+  })
+    .setClassToggle('#learn-how .animate-text-fade-up', 'text-fade-up')
+    .addTo(controller);
+  // Section Two
+  var overviewScene = new ScrollMagic.Scene({
+    triggerElement: '#benefits',
+    reverse: false
+  })
+    .setClassToggle('#benefits .animate-text-fade-up', 'text-fade-up')
+    .addTo(controller);
+  var overviewScene = new ScrollMagic.Scene({
+    triggerElement: '#benefits',
+    reverse: false
+  })
+    .setClassToggle('#benefits .animate-benefits-image', 'benefits-image-fade-up')
+    .addTo(controller);
+  // Section Three
+  var overviewScene = new ScrollMagic.Scene({
+    triggerElement: '#how-to',
+    reverse: false
+  })
+    .setClassToggle('#how-to .animate-text-fade-up', 'text-fade-up')
+    .addTo(controller);
+  var overviewScene = new ScrollMagic.Scene({
+    triggerElement: '#how-to',
+    reverse: false
+  })
+    .setClassToggle('#how-to .animate-text-fade-up-delay', 'text-fade-up-delay')
+    .addTo(controller);
+  // Section Four
+  var overviewScene = new ScrollMagic.Scene({
+    triggerElement: '#overview-why',
+    reverse: false
+  })
+    .setClassToggle('#overview-why .animate-text-fade-up', 'text-fade-up')
+    .addTo(controller);
+  // Section Five
+  var overviewScene = new ScrollMagic.Scene({
+    triggerElement: '#overview-vision',
+    reverse: false
+  })
+    .setClassToggle('#overview-vision .animate-text-fade-up', 'text-fade-up')
+    .addTo(controller);
+  // Section Six
+  var overviewScene = new ScrollMagic.Scene({
+    triggerElement: '#race-to-top',
+    reverse: false
+  })
+    .setClassToggle('#race-to-top .animate-text-fade-up', 'text-fade-up')
+    .addTo(controller);
+  // End Overview Animations
 
+
+  // Begin Contribute Animations
+  // Section One
+  var contributeScene = new ScrollMagic.Scene({
+    triggerElement: '#help-build',
+    reverse: false
+  })
+    .setClassToggle('#help-build .animate-text-fade-up', 'text-fade-up')
+    .addTo(controller);
+  // Section Two
+  var contributeScene = new ScrollMagic.Scene({
+    triggerElement: '#tds-values',
+    reverse: false
+  })
+    .setClassToggle('#tds-values .animate-text-fade-up', 'text-fade-up')
+    .addTo(controller);
+  // Section Three
+  var contributeScene = new ScrollMagic.Scene({
+    triggerElement: '#current-partners',
+    reverse: false
+  })
+    .setClassToggle('#current-partners .animate-text-fade-up', 'text-fade-up')
+    .addTo(controller);
+  // Section Four
+  var contributeScene = new ScrollMagic.Scene({
+    triggerElement: '#share-feedback',
+    reverse: false
+  })
+    .setClassToggle('#share-feedback .animate-text-fade-up', 'text-fade-up')
+    .addTo(controller);
+  // End Contribute Animations
 
 
 
