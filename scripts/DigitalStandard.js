5b3663f7a24e0c1ad3ddcc9aa759b2472cb75a97
tombeck <t@ocupop.com>
2020-09-04 09:12:26 -0700

    added csv download and file cleanup

---
+++ scripts/DigitalStandard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+const DigitalStandard = () => {
+  return (
+    <div>
+      The Standard
+    </div>
+  )
+}
+
+DigitalStandard.propTypes = {
+
+}
+
+export default DigitalStandard
