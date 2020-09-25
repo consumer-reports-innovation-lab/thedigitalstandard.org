238b089e744cc7f8861e58483f1799cb2300574b
tombeck <t@ocupop.com>
2020-09-25 07:57:53 -0700

    case studies filtering

--- scripts/CaseStudies.js
+++ scripts/CaseStudies.js
@@ -2,37 +2,46 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 const queryString = window.location.search
 const urlParams = new URLSearchParams(queryString)
-import Filters from "./Filters"
 
 const CaseStudies = () => {
   const { history } = window
-  const org = urlParams.get('org') || ''
-  const status = urlParams.get('status') || ''
+  const filterByOrg = urlParams.get('filterByOrg') || ''
+  const filterByStatus = urlParams.get('filterByStatus') || ''
   const [state, setState] = useState({
     studies: [],
+    organizations: [],
+    statuses: [],
     filteredStudies: [],
     isFiltered: false,
-    org: org,
-    status: status
+    filterByOrg: filterByOrg,
+    filterByStatus: filterByStatus
   })
 
   useEffect(async () => {
     const studies = await axios('/case-studies/data.json')
+    const organizations = [...new Set(studies.data.map(study => study.lead_organization))]
+    const statuses = [...new Set(studies.data.map(study => study.status))]
     const filteredStudies = studies.data.filter(study => true)
     const isFiltered = studies.length !== filteredStudies.length ? true : false
-
-    setState({ ...state, studies: studies.data, filteredStudies: filteredStudies, isFiltered: isFiltered })
+    setState({
+      ...state,
+      studies: studies.data,
+      filteredStudies: filteredStudies,
+      isFiltered: isFiltered,
+      organizations: organizations,
+      statuses: statuses
+    })
   }, [])
 
   useEffect(() => {
-    const { studies, org, status } = state
-    const filteredStudies = filterStudies({ studies, org, status })
+    const { studies, filterByOrg, filterByStatus } = state
+    const filteredStudies = filterStudies({ studies, filterByOrg, filterByStatus })
 
-    history.pushState({}, "", `?org=${org}&status=${status}`)
+    history.pushState({}, "", `?filterByOrg=${filterByOrg}&filterByStatus=${filterByStatus}`)
 
     setState({ ...state, filteredStudies })
 
-  }, [state.org, state.status])
+  }, [state.filterByOrg, state.filterByStatus])
 
   function filterStudies({ studies, filterByOrg = false, filterByStatus = false }) {
     let filteredStudies = studies.filter(study => {
@@ -53,7 +62,7 @@ const CaseStudies = () => {
 
   return (
     <>
-      <section className="bg-secondary">
+      <section className="bg-secondary overflow-visible">
         <div className="container">
           <div className="row">
             <div className="col-12">
@@ -64,26 +73,30 @@ const CaseStudies = () => {
             <div className="col-12 col-sm-6 d-flex">
               <div className="dropdown">
                 <button className="btn btn-secondary dropdown-toggle blue-button p-3 small-text" type="button" data-toggle="dropdown">
-                  Filter by lead org
-              </button>
+                  {state.filterByOrg || 'Filter by Organization'}
+                </button>
                 <div className="dropdown-menu">
-                  <a className="dropdown-item" href="#">Consumer Reports</a>
+                  <button className="dropdown-item" onClick={() => setState({ ...state, filterByOrg: false })}>All</button>
+                  {state.organizations && state.organizations.map(organization => (
+                    <button className="dropdown-item" onClick={() => setState({ ...state, filterByOrg: organization })}>{organization}</button>
+                  ))}
                 </div>
               </div>
               <div className="dropdown ml-lg-3">
                 <button className="btn btn-secondary dropdown-toggle blue-button p-3 small-text" type="button" data-toggle="dropdown">
-                  Filter by Status
-              </button>
+                  {state.filterByStatus || 'Filter by Status'}
+                </button>
                 <div className="dropdown-menu">
-                  <a className="dropdown-item" href="#">Well understood</a>
-                  <a className="dropdown-item" href="#">Under development</a>
-                  <a className="dropdown-item" href="#">Under discussion</a>
+                  <button className="dropdown-item" onClick={() => setState({ ...state, filterByStatus: false })}>All</button>
+                  {state.statuses && state.statuses.map(status => (
+                    <button className="dropdown-item" onClick={() => setState({ ...state, filterByStatus: status })}>{status}</button>
+                  ))}
                 </div>
               </div>
             </div>
             <div className="col-12 col-sm-6 d-flex align-items-center justify-content-end">
-              <a href="#" className="casestudy-sort">A - Z</a>
-              <a href="#" className="ml-lg-3 casestudy-sort">Recent</a>
+              {/* <a href="#" className="casestudy-sort">A - Z</a>
+              <a href="#" className="ml-lg-3 casestudy-sort">Recent</a> */}
             </div>
           </div>
         </div>
