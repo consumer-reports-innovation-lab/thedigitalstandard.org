480fd822230b1555d10c6b247fa49c3f0ca874bf
tombeck <t@ocupop.com>
2020-10-19 11:35:41 -0700

    adjust organization reference to launch

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,4 @@
-export const organization = 'ocupop'
+export const organization = 'TheDigitalStandard'
 export const repository = 'TheDigitalStandard'
 export const api_url = `https://${organization}.github.io/${repository}/`
 export const release_url = `https://api.github.com/repos/${organization}/${repository}/releases`
